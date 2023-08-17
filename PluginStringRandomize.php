a8f7ae0780d024c9489a146ea1634c7f7d048cd2
Claes Östlund <claes@stenbergit.se>
2023-08-17 17:41:15 +0200

    1.1.0

--- PluginStringRandomize.php
+++ PluginStringRandomize.php
@@ -3,7 +3,7 @@ class PluginStringRandomize{
   public function randomize($length = 10, $characters = 'abcdefghijklmnopqrstuvxyz'){
     $randomString = ''; 
     for ($i = 0; $i < $length; $i++) { 
-      $index = rand(0, strlen($characters) - 1); 
+      $index = rand(0, wfPhpfunc::strlen($characters) - 1); 
       $randomString .= $characters[$index]; 
     }
     return $randomString;
